7b29285917944cb2a0f3775876068b426a73db9a
Manuel Ottlik <manuel.ottlik@web.de>
2019-12-10 14:24:47 +0100

    add action

--- f3t-build-docker-image/entrypoint.sh
+++ f3t-build-docker-image/entrypoint.sh
@@ -4,16 +4,15 @@
 printf "@edact:registry=$INPUT_NPM_REGISTRY_URL \n \
           //npm.pkg.github.com/:_authToken=${REGISTRY_TOKEN} \n \
           @fortawesome:registry=https://npm.fontawesome.com/ \n \
-          //npm.fontawesome.com/:_authToken=$FONTAWESOME_TOKEN"> .npmrc
+          //npm.fontawesome.com/:_authToken=${FONTAWESOME_TOKEN}"> .npmrc
 
-npm ci
+docker login ${INPUT_DOCKER_REGISTRY_URL} -u publisher -p "${REGISTRY_TOKEN}"
 
-npm run build
-      
-        
-V=$(echo $GITHUB_REF| cut -d'/' -f 3)
-VERSION=$(echo $V| cut -d'v' -f 2)
+docker build \
+    --build-arg REGISTRY_TOKEN=${REGISTRY_TOKEN} \
+    --build-arg FONTAWESOME_TOKEN=${FONTAWESOME_TOKEN} \
+    -t tempcontainer .
 
-npm --no-git-tag-version version $VERSION --force
+docker tag tempcontainer ${INPUT_DOCKER_REGISTRY_URL}${GITHUB_REPOSITORY}${IMAGE_NAME}:${IMAGE_TAG}
 
-npm publish
+docker push ${INPUT_DOCKER_REGISTRY_URL}${GITHUB_REPOSITORY}${IMAGE_NAME}
