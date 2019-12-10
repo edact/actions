47bcf81e7bb398bdcd7f48ffbfbb8cb48e06bfc9
Manuel Ottlik <manuel.ottlik@web.de>
2019-12-10 17:33:55 +0100

    :recycle: remove .npmrc creation

--- f3t-build-docker-image/entrypoint.sh
+++ f3t-build-docker-image/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -l
 
-
-printf "@edact:registry=https://${INPUT_NPM_REGISTRY_URL} \n \
-          //${INPUT_NPM_REGISTRY_URL}/:_authToken=${REGISTRY_TOKEN} \n \
-          @fortawesome:registry=https://npm.fontawesome.com/ \n \
-          //npm.fontawesome.com/:_authToken=${FONTAWESOME_TOKEN}"> .npmrc
-
 docker login ${INPUT_DOCKER_REGISTRY_URL} -u publisher -p "${REGISTRY_TOKEN}"
 
 docker build \
