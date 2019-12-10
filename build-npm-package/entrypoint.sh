d907329517bfddc5ab326bc26cc86076f3a52d1d
Manuel Ottlik <manuel.ottlik@web.de>
2019-12-10 13:41:30 +0100

    a

--- build-npm-package/entrypoint.sh
+++ build-npm-package/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-printf '@edact:registry=${1} \n \
+printf '@edact:registry=$1 \n \
           //npm.pkg.github.com/:_authToken=${REGISTRY_TOKEN} \n \
           @fortawesome:registry=https://npm.fontawesome.com/ \n \
           //npm.fontawesome.com/:_authToken=${FONTAWESOME_TOKEN}'> .npmrc
