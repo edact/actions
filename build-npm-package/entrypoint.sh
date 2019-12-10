40c14c22a6c71b131069792e3175817f3225b8ae
Manuel Ottlik <manuel.ottlik@web.de>
2019-12-10 16:53:03 +0100

    :wrench: add slash

--- build-npm-package/entrypoint.sh
+++ build-npm-package/entrypoint.sh
@@ -2,7 +2,7 @@
 
 
 printf "@edact:registry=https://$INPUT_NPM_REGISTRY_URL/ \n \
-          //${INPUT_NPM_REGISTRY_URL}:_authToken=${REGISTRY_TOKEN} \n \
+          //${INPUT_NPM_REGISTRY_URL}/:_authToken=${REGISTRY_TOKEN} \n \
           @fortawesome:registry=https://npm.fontawesome.com/ \n \
           //npm.fontawesome.com/:_authToken=$FONTAWESOME_TOKEN"> .npmrc
 
