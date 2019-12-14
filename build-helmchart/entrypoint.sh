f323c1ef97dfe1203ee501a985bc7fa2b98beb34
janMahn <mahn@edact.de>
2019-12-14 17:52:38 +0100

    added build-helmchart
    new action to deal with helm charts (v3).

---
+++ build-helmchart/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+
+export HELM_EXPERIMENTAL_OCI=1
+
+# switch to working directory
+cd ${INPUT_WORKING_DIRECTORY}
+
+# log into helm registry
+helm registry login -u "${INPUT_REGISTRY_USER}" -p "${INPUT_REGISTRY_TOKEN}" ${INPUT_HELM_REGISTRY_URL}
+
+# split chart tags in array
+chart_tags=$(echo $INPUT_CHART_TAGS | tr ", " "\n")
+
+# set tags
+for chart_tag in $chart_tags
+do 
+    helm chart save . ${INPUT_HELM_REGISTRY_URL}/${INPUT_REPOSITORY_NAME}/${INPUT_CHART_NAME}:${chart_tag}
+    helm chart push ${INPUT_HELM_REGISTRY_URL}/${INPUT_REPOSITORY_NAME}/${INPUT_CHART_NAME}:${chart_tag}
+done
