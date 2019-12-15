ec8a4c38a06c54533abc154ba436157c0b6cf564
Manuel Ottlik <manuel.ottlik@web.de>
2019-12-15 18:07:44 +0100

    :house: adapt attribute names

--- build-helmchart/entrypoint.sh
+++ build-helmchart/entrypoint.sh
@@ -6,7 +6,7 @@ export HELM_EXPERIMENTAL_OCI=1
 cd ${INPUT_WORKING_DIRECTORY}
 
 # log into helm registry
-helm registry login -u "${INPUT_REGISTRY_USER}" -p "${INPUT_REGISTRY_TOKEN}" ${INPUT_HELM_REGISTRY_URL}
+helm registry login -u "${INPUT_HELM_REGISTRY_USER}" -p "${INPUT_HELM_REGISTRY_TOKEN}" ${INPUT_HELM_REGISTRY_URL}
 
 # split chart tags in array
 chart_tags=$(echo $INPUT_CHART_TAGS | tr ", " "\n")
@@ -14,6 +14,6 @@ chart_tags=$(echo $INPUT_CHART_TAGS | tr ", " "\n")
 # set tags
 for chart_tag in $chart_tags
 do 
-    helm chart save . ${INPUT_HELM_REGISTRY_URL}/${INPUT_REPOSITORY_NAME}/${INPUT_CHART_NAME}:${chart_tag}
-    helm chart push ${INPUT_HELM_REGISTRY_URL}/${INPUT_REPOSITORY_NAME}/${INPUT_CHART_NAME}:${chart_tag}
+    helm chart save . ${INPUT_HELM_REGISTRY_URL}/helm/${GITHUB_REPOSITORY}/${INPUT_CHART_NAME}:${chart_tag}
+    helm chart push ${INPUT_HELM_REGISTRY_URL}/helm/${GITHUB_REPOSITORY}/${INPUT_CHART_NAME}:${chart_tag}
 done
