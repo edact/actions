d4c565eae61bd1989dd13151ced303128726247e
janMahn <34416075+janMahn@users.noreply.github.com>
2020-04-25 16:56:14 +0200

    Update entrypoint.sh

--- create-authjson-file/entrypoint.sh
+++ create-authjson-file/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 JSON_FMT='{"github-oauth":{"%s": "%s"} }\n'
-printf "$JSON_FMT" "$INPUT_REGISTRY_URL" "$REGISTRY_TOKEN" > auth.json
+printf "$JSON_FMT" "$INPUT_REGISTRY_URL" "$PHP_DEPLOY_TOKEN" > auth.json
