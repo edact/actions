6834db5a0e6843401abf83f40244354350a58c0d
Manuel Ottlik <manuel.ottlik@web.de>
2020-05-12 10:52:33 +0200

    remove $

--- bundle-openapi/entrypoint.sh
+++ bundle-openapi/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 # find out folder name by repo name
-$FOLDER=$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)
+FOLDER=$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)
 
 # compute input and output path if not given
 if [ "${INPUT_INPUT_PATH}"=="defaultpath" ]
