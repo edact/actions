b40a562b72882000be4b20a39c4e2262139578ef
Manuel Ottlik <manuel.ottlik@web.de>
2020-05-12 10:46:51 +0200

    remove whitespace

--- bundle-openapi/entrypoint.sh
+++ bundle-openapi/entrypoint.sh
@@ -1,19 +1,14 @@
 #!/bin/sh -l
 
-echo ${INPUT_INPUT_PATH}
-echo ${INPUT_OUTPUT_PATH}
-echo ${INPUT_OUTPUT_FILETYPE}
-echo ${INPUT_DELETE_GLOB}
-
 # find out folder name by repo name
 $FOLDER=$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)
 
 # compute input and output path if not given
-if [ "${INPUT_INPUT_PATH}" == "defaultpath" ]
+if [ "${INPUT_INPUT_PATH}"=="defaultpath" ]
     then $INPUT_INPUT_PATH="$FOLDER/api/public/openapi.yml"
 fi
 
-if [ "${INPUT_OUTPUT_PATH}" == "defaultpath" ]
+if [ "${INPUT_OUTPUT_PATH}"=="defaultpath" ]
     then $INPUT_OUTPUT_PATH="$FOLDER/api/public/openapi.yml"
 fi
 
