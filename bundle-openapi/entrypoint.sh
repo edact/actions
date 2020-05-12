951720c594bfb32458217853ee236dc0e4fd389b
Manuel Ottlik <manuel.ottlik@web.de>
2020-05-12 10:35:49 +0200

    prefix with INPUT_

--- bundle-openapi/entrypoint.sh
+++ bundle-openapi/entrypoint.sh
@@ -1,23 +1,21 @@
 #!/bin/sh -l
-GITHUB_REPOSITORY="edact/a3t-provider-sql"
-INPUT_PATH="defaultpath"
 
 # find out folder name by repo name
 FOLDER=$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)
 
 # compute input and output path if not given
-if [ "$INPUT_PATH" == "defaultpath" ]
-    then INPUT_PATH="$FOLDER/api/public/openapi.yml"
+if [ "$INPUT_INPUT_PATH" == "defaultpath" ]
+    then INPUT_INPUT_PATH="$FOLDER/api/public/openapi.yml"
 fi
 
-if [ "$OUTPUT_PATH" == "defaultpath" ]
-    then OUTPUT_PATH="$FOLDER/api/public/openapi.yml"
+if [ "$INPUT_OUTPUT_PATH" == "defaultpath" ]
+    then INPUT_OUTPUT_PATH="$FOLDER/api/public/openapi.yml"
 fi
 
 # bundle
-# npx -p @apidevtools/swagger-cli swagger-cli bundle $INPUT_PATH -o $OUTPUT_PATH -t $OUTPUT_FILETYPE 
+npx -p @apidevtools/swagger-cli swagger-cli bundle $INPUT_INPUT_PATH -o $INPUT_OUTPUT_PATH -t $INPUT_OUTPUT_FILETYPE 
 
 # delete sub files by glob
-DIR=$(dirname "$INPUT_PATH")
+DIR=$(dirname "$INPUT_INPUT_PATH")
 cd $DIR
-rm $DELETE_GLOB
+rm $INPUT_DELETE_GLOB
