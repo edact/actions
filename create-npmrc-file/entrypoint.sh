d14ff893cf05d75bd82dac223658bd371f097532
Manuel Ottlik <manuel.ottlik@web.de>
2020-08-27 13:53:27 +0200

    remove fa5 from npmrc (#10)
    * remove fa5 registry
    
    * add token as param

--- create-npmrc-file/entrypoint.sh
+++ create-npmrc-file/entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/sh -l
 
-printf "@edact:registry=https://$INPUT_NPM_REGISTRY_URL/ \n \
-          //${INPUT_NPM_REGISTRY_URL}/:_authToken=${REGISTRY_TOKEN} \n \
-          @fortawesome:registry=https://npm.fontawesome.com/ \n \
-          //npm.fontawesome.com/:_authToken=$FONTAWESOME_TOKEN"> .npmrc
+printf "${INPUT_NPM_SCOPE}:registry=https://${INPUT_NPM_REGISTRY_URL}/ \n //${INPUT_NPM_REGISTRY_URL}/:_authToken=${REGISTRY_TOKEN}"> .npmrc
