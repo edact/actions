3e0c5f113af2b53d75643c5c7230d3277bd441fa
Manuel Ottlik <manuel.ottlik@web.de>
2020-08-27 17:18:15 +0200

    q

--- lint-openapi/entrypoint.sh
+++ lint-openapi/entrypoint.sh
@@ -16,7 +16,7 @@ fi
 echo "try one last time"
 
 # lint
-{ npx -p @stoplight/spectral spectral lint e3t-module-school/api/public/openapi.yml --skip-rule oas3-unused-components-schema } >> spectral.txt
+{ npx -p @stoplight/spectral spectral lint e3t-module-school/api/public/openapi.yml --skip-rule oas3-unused-components-schema; } >> spectral.txt
 
 RESULT=$(cat spectral.txt)
 
