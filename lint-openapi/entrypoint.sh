c6bf1a60156c8c48131f7d0163a406a904053388
Manuel Ottlik <manuel.ottlik@web.de>
2020-08-27 17:10:55 +0200

    tye

--- lint-openapi/entrypoint.sh
+++ lint-openapi/entrypoint.sh
@@ -13,17 +13,11 @@ if [ ! -f "$INPUT_INPUT_PATH" ]; then
     exit 1
 fi
 
-echo "tryyy"
+echo "try last time"
 
 # lint
-npx -p @stoplight/spectral spectral lint e3t-module-school/api/public/openapi.yml --skip-rule oas3-unused-components-schema > spectral.txt
+npx -p @stoplight/spectral spectral lint e3t-module-school/api/public/openapi.yml --skip-rule oas3-unused-components-schema > spectral.txt 2>&1
 
 RESULT=$(cat spectral.txt)
-
-echo "Hallo Welt" > test.txt
-
-R=$(cat test.txt)
-
-echo "::warning::$R"
 
 echo "::warning::$RESULT"
