f66c3735ca146a477368eab56b82268481b382b5
Manuel Ottlik <manuel.ottlik@web.de>
2020-09-07 16:38:53 +0200

    use input path (#16)

--- lint-openapi/entrypoint.sh
+++ lint-openapi/entrypoint.sh
@@ -14,4 +14,4 @@ if [ ! -f "$INPUT_INPUT_PATH" ]; then
 fi
 
 # lint
-npx -p @stoplight/spectral spectral lint e3t-module-school/api/public/openapi.yml --skip-rule oas3-unused-components-schema
+npx -p @stoplight/spectral spectral lint ${INPUT_INPUT_PATH} --skip-rule oas3-unused-components-schema
