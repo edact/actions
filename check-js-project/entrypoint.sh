79a71254419b41793ea6dd781d4cf9868352cdcb
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2020-12-04 13:35:48 +0100

    add log grouping (#19)

--- check-js-project/entrypoint.sh
+++ check-js-project/entrypoint.sh
@@ -4,13 +4,19 @@
 # rm package-lock.json -f
 
 # install dependencies
+echo "::group::Install dependencies"
 npm install
+echo "::endgroup::"
 
 # run tests specified in package.json
+echo "::group::Run tests"
 npm run test --if-present
+echo "::endgroup::"
 
 # set mode to production
 export NODE_ENV="production"
 
 # lint project
+echo "::group::Run linter"
 npm run lint --if-present --no-fix
+echo "::endgroup::"
