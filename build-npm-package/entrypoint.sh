5cf8cba164812076af7d67ed3a05255143dbeb4e
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2021-01-13 18:55:52 +0100

    Update entrypoint.sh

--- build-npm-package/entrypoint.sh
+++ build-npm-package/entrypoint.sh
@@ -22,5 +22,5 @@ npm --no-git-tag-version version $INPUT_PACKAGE_VERSION --force
 
 # publish package to registry
 echo "::group::Publish package"
-npm publish
+npm publish --registry=https://npm.pkg.github.com
 echo "::endgroup::"
