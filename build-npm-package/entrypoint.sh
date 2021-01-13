f2112d7ded11b31aba9c9f3a82c2baa761f160b9
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2021-01-13 18:39:10 +0100

    Update entrypoint.sh

--- build-npm-package/entrypoint.sh
+++ build-npm-package/entrypoint.sh
@@ -3,6 +3,8 @@
 # makes the script existing once an error occours
 set -e
 
+npm adduser --registry=https://npm.pkg.github.com --scope=@edact
+
 # install dependencies
 echo "::group::Install package dependencies"
 npm ci
@@ -18,5 +20,5 @@ npm --no-git-tag-version version $INPUT_PACKAGE_VERSION --force
 
 # publish package to registry
 echo "::group::Publish package"
-npm publish --registry npm.pkg.github.com/edact/f3t-vue-components
+npm publish --registry https://npm.pkg.github.com
 echo "::endgroup::"
