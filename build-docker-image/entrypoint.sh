7f940eec3b7dc184d221e8bf83dcb4e51029d5c0
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2021-02-04 13:26:01 +0100

    try caching

--- build-docker-image/entrypoint.sh
+++ build-docker-image/entrypoint.sh
@@ -9,11 +9,17 @@ cd ${INPUT_WORKING_DIRECTORY}
 # log into docker registry
 echo ${INPUT_DOCKER_REGISTRY_TOKEN} | docker login -u ${INPUT_DOCKER_REGISTRY_USER} --password-stdin ${INPUT_DOCKER_REGISTRY_URL}
 
+FULL_IMAGE_NAME=${INPUT_DOCKER_REGISTRY_URL}/${GITHUB_REPOSITORY}/${INPUT_IMAGE_NAME}
+
+# pull image for caching
+docker pull ${FULL_IMAGE_NAME}
+
 # build image
 echo "::group::Build image"
 docker build \
     --build-arg=DOCKER_REGISTRY_URL=${INPUT_DOCKER_REGISTRY_URL} \
     --build-arg=BASE_TAG=${INPUT_BUILD_BASE_TAG} \
+    --cache-from=${FULL_IMAGE_NAME}
     -t tempcontainer:latest .
 echo "::endgroup::"
 
@@ -24,7 +30,7 @@ image_tags=$(echo $INPUT_IMAGE_TAGS | tr ", " "\n")
 for image_tag in $image_tags
 do
     echo $image_tag
-    docker tag tempcontainer:latest ${INPUT_DOCKER_REGISTRY_URL}/${GITHUB_REPOSITORY}/${INPUT_IMAGE_NAME}:${image_tag}    
+    docker tag tempcontainer:latest ${FULL_IMAGE_NAME}:${image_tag}    
 done
 
 # push image to registry
