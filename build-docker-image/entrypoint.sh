3887950dfadd6a929867b7c3458967dffacec43f
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2021-02-04 16:19:51 +0100

    rebuild

--- build-docker-image/entrypoint.sh
+++ build-docker-image/entrypoint.sh
@@ -13,42 +13,52 @@ FULL_IMAGE_NAME=${INPUT_DOCKER_REGISTRY_URL}/${GITHUB_REPOSITORY}/${INPUT_IMAGE_
 
 # split image tags in array
 IMAGE_TAGS=$(echo $INPUT_IMAGE_TAGS | tr ", " "\n")
-FIRST_IMAGE_TAG=$(echo $INPUT_IMAGE_TAGS | cut -f1 -d",")
+CACHE_TAGS=$(echo $INPUT_CACHE_TAGS | tr ", " "\n")
+#FIRST_IMAGE_TAG=$(echo $INPUT_IMAGE_TAGS | cut -f1 -d",")
+
+for CACHE_TAG in $CACHE_TAGS
+do
+    docker pull ${FULL_IMAGE_NAME}:${CACHE_TAG} --quiet || true
+    CACHE_FROM_STRING=$CACHE_FROM_STRING+" --cache-from=${FULL_IMAGE_NAME}:${CACHE_TAG}"
+done
 
 # pull image for caching
-if [ "$INPUT_USE_CACHE" = true ] ; then
-    echo "::group::Pull cached image"
-    docker pull ${FULL_IMAGE_NAME}:${FIRST_IMAGE_TAG} --quiet || true
-    echo "::endgroup::"
-fi
+# if [ "$INPUT_USE_CACHE" = true ] ; then
+#     echo "::group::Pull cached image"
+#     docker pull ${FULL_IMAGE_NAME}:${FIRST_IMAGE_TAG} --quiet || true
+#     echo "::endgroup::"
+# fi
 
 # pull image for caching
-if [ -n "$INPUT_CACHE_BUILD_STAGE" ]; then
-    echo "::group::Build cache image"
-    BUILD_STAGE_IMAGE_TAG="cache-${INPUT_CACHE_BUILD_STAGE}"
-    docker pull ${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG} --quiet || true
+# if [ -n "$INPUT_CACHE_BUILD_STAGE" ]; then
+#     echo "::group::Build cache image"
+#     BUILD_STAGE_IMAGE_TAG="cache-${INPUT_CACHE_BUILD_STAGE}"
+#     docker pull ${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG} --quiet || true
 
-    docker build \
-    --target ${INPUT_CACHE_BUILD_STAGE} \
-    --build-arg=DOCKER_REGISTRY_URL=${INPUT_DOCKER_REGISTRY_URL} \
-    --build-arg=BASE_TAG=${INPUT_BUILD_BASE_TAG} \
-    --cache-from=${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG} \
-    -t ${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG} .
+#     # docker build \
+#     # --target ${INPUT_CACHE_BUILD_STAGE} \
+#     # --build-arg=DOCKER_REGISTRY_URL=${INPUT_DOCKER_REGISTRY_URL} \
+#     # --build-arg=BASE_TAG=${INPUT_BUILD_BASE_TAG} \
+#     # --cache-from=${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG} \
+#     # -t ${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG} .
 
-    docker push ${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG}
-    echo "::endgroup::"
-fi
+#     # docker push ${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG}
+#     echo "::endgroup::"
+# fi
 
 # build image
 echo "::group::Build image"
 docker build \
     --build-arg=DOCKER_REGISTRY_URL=${INPUT_DOCKER_REGISTRY_URL} \
     --build-arg=BASE_TAG=${INPUT_BUILD_BASE_TAG} \
-    $( (("$INPUT_USE_CACHE" = true)) && printf %s "--cache-from=${FULL_IMAGE_NAME}:${FIRST_IMAGE_TAG}") \
-    $( (("$INPUT_USE_CACHE" = true)) && ((-n "$INPUT_CACHE_BUILD_STAGE")) && printf %s "--cache-from=${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG}") \
-    --cache-from=${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG} \
+    $( (($INPUT_TARGET_STAGE)) && printf %s "--target $INPUT_TARGET_STAGE") \
+    $( (($INPUT_CACHE_TAGS)) && printf %s $CACHE_FROM_STRING) \
     -t tempcontainer:latest .
 echo "::endgroup::"
+
+# $( (("$INPUT_USE_CACHE" = true)) && printf %s "--cache-from=${FULL_IMAGE_NAME}:${FIRST_IMAGE_TAG}") \
+#     $( (("$INPUT_USE_CACHE" = true)) && ((-n "$INPUT_CACHE_BUILD_STAGE")) && printf %s "--cache-from=${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG}") \
+#     --cache-from=${FULL_IMAGE_NAME}:${BUILD_STAGE_IMAGE_TAG} \
 
 # set tags
 for IMAGE_TAG in $IMAGE_TAGS
