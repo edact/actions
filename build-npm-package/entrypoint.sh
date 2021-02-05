987c3c44bccacf12b793f6753aab309d3618213f
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2021-02-05 09:48:42 +0100

    remove publishing

--- build-npm-package/entrypoint.sh
+++ build-npm-package/entrypoint.sh
@@ -3,25 +3,12 @@
 # makes the script existing once an error occours
 set -eu
 
-#npm adduser --registry=https://npm.pkg.github.com --scope=@edact
-#npm whoami
-#exit
-
 # install dependencies
 echo "::group::Install package dependencies"
-npm ci
+npm install
 echo "::endgroup::"
 
 # build for production
 echo "::group::Build package"
 npm run build
-echo "::endgroup::"
-
-# set version
-npm --no-git-tag-version version $INPUT_PACKAGE_VERSION --force
-
-# publish package to registry
-echo "::group::Publish package"
-# TODO: remove --registry
-npm publish --registry=https://npm.pkg.github.com
 echo "::endgroup::"
