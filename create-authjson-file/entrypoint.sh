f5eaedfb7f9328e2070b22601293f25a98a65cb7
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2021-03-05 13:56:48 +0100

    Update entrypoint.sh

--- create-authjson-file/entrypoint.sh
+++ create-authjson-file/entrypoint.sh
@@ -4,5 +4,5 @@
 set -euo pipefail
 
 JSON_FMT='{"github-oauth":{"%s": "%s"} }\n'
-printf "$JSON_FMT" "$INPUT_REGISTRY_URL" "$PHP_DEPLOY_TOKEN" > auth.json
+printf "$JSON_FMT" "$INPUT_REGISTRY_URL" "$INPUT_DEPLOY_TOKEN" > auth.json
 mv auth.json ./$INPUT_LOCATION
